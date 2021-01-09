acfdbfe4ee994e43c751ec9e4f33dcae2e3a5ba7
Jesse Parsons <savvyjesse@aol.com>
2021-01-09 12:16:22 -0500

    add some color

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
+use colored::*;
 use std::io;
-use std::{thread, time}; // debugging purposes
-                         // TODO
-                         // variable to track player turn ✔️
-                         // function to get desired column to 'drop' piece from player ✔️
-                         // function to 'drop' piece into column if it is available ✔️
-                         // function to check win
-                         // function to change active player ✔️
-                         // game loop ✔️
+use std::{thread, time};
+// TODO
+// variable to track player turn ✔️
+// function to get desired column to 'drop' piece from player ✔️
+// function to 'drop' piece into column if it is available ✔️
+// add color (and spaghetti) ✔️
+// function to check win
+// function to change active player ✔️
+// game loop ✔️
 
 #[allow(dead_code)]
 fn main() {
     ////////////// VARIABLE INITIALIZATION //////////////
     let mut current_player: u8 = 1;
-    let mut board = vec![vec!["O"; 7]; 5]; // 7X5
+    let mut board = vec![vec!["O".black().on_green(); 7]; 5]; // 7X5
 
     ////////////// USER FUNCTIONS //////////////
     fn change_player(_current_player: &mut u8) {
@@ -29,8 +31,10 @@ fn main() {
             let mut choice = String::new();
             io::stdin().read_line(&mut choice).expect("Please input");
             let _choice: u8 = match choice.trim().parse() {
-                // logic here to make sure num is in 0..6
-                Ok(num) => break num,
+                Ok(num) => match num {
+                    0..=6 => break num,
+                    _ => continue,
+                },
                 Err(_) => {
                     println!("Please input a number");
                     continue;
@@ -42,25 +46,33 @@ fn main() {
     ////////////// GAME BOARD FUNCTIONS //////////////
     fn welcome_message() {
         println!("Welcome to Connect 4!");
+        println!("Player 1 will be {}.", "red".red());
+        println!("Player 2 will be {}.", "blue".blue());
     }
 
-    fn print_board(board: &Vec<Vec<&str>>) {
+    fn print_board(board: &Vec<Vec<ColoredString>>) {
         for row in board.iter() {
             for cell in row.iter() {
-                print!("  {}  ", cell);
+                print!(
+                    "{}{}{}",
+                    " ".black().on_green(),
+                    cell,
+                    " ".black().on_green()
+                );
             }
             print!("\n"); // add new line after every row
         }
+        println!(" 0  1  2  3  4  5  6");
     }
 
     fn clear_screen() {
         std::process::Command::new("clear").status().unwrap();
     }
 
-    fn process_choice(choice: u8, board: &mut Vec<Vec<&str>>, player: &mut u8) {
+    fn process_choice(choice: u8, board: &mut Vec<Vec<ColoredString>>, player: &mut u8) {
         let choice: usize = choice as usize;
         // get new choice and rerun function if column is filled
-        if board[0][choice] != "O" {
+        if board[0][choice] != "O".black().on_green() {
             println!("That column is filled!");
             thread::sleep(time::Duration::from_secs(2));
             process_choice(get_desired_choice(player), board, player);
@@ -68,20 +80,28 @@ fn main() {
         }
         for row in 0..board.len() {
             // if on last row && empty, place piece
-            if row == 4 && board[row][choice] == "O" {
-                board[row][choice] = "X";
+            if row == 4 && board[row][choice] == "O".black().on_green() {
+                match player {
+                    1 => board[row][choice] = "X".red(),
+                    2 => board[row][choice] = "X".blue(),
+                    _ => (),
+                }
                 return;
-            } else if board[row][choice] != "O" {
+            } else if board[row][choice] != "O".black().on_green() {
                 // if the column isnt filled, place piece
                 // 1 row above existing piece
-                board[row - 1][choice] = "X";
+                match player {
+                    1 => board[row - 1][choice] = "X".red(),
+                    2 => board[row - 1][choice] = "X".blue(),
+                    _ => (),
+                }
                 return;
             }
         }
     }
 
-    fn game_loop(board: &mut Vec<Vec<&str>>, current_player: &mut u8) {
-        let mut running = false;
+    fn game_loop(board: &mut Vec<Vec<ColoredString>>, current_player: &mut u8) {
+        let mut _running = false;
         clear_screen();
         welcome_message();
         loop {
